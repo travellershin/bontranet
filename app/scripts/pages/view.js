7a5bc19302f19ffb568888ab52939bcc8f892aee
unknown <travellershin@gmail.com>
2018-08-26 20:55:30 +0900

    180826

--- app/scripts/pages/view.js
+++ app/scripts/pages/view.js
@@ -1,4 +1,5 @@
 import View_Metro from "./view/metro.js";
+import View_Spot from "./view/spot.js";
 
 let View = {
     map:{},
@@ -8,6 +9,7 @@ let View = {
         firebase.database().ref("cities/"+cid).once("value", snap => {
             let data = snap.val();
             View_Metro.init(data);
+            View_Spot.init(data);
             this.data = data;
             toast("도시정보 로딩 완료");
         });
@@ -20,8 +22,23 @@ let View = {
         $(".view").on("click", ".metro_disable", function(){
             View_Metro.disable();
         });
-        $(".view").on("click", ".metro_click", function(){
+        $(".view").on("click", ".click_metro", function(){
             View_Metro.clickMode();
+        });
+        $(".view").on("click", ".click_metroSpot", function(){
+            View_Metro.clickSpotMode();
+        });
+        $(".view").on("focusout", ".setter__radius__input", function(){
+            View_Metro.changeRadius();
+        });
+        $(".view").on("click", ".spot_full", function(){
+            View_Spot.full();
+        });
+        $(".view").on("click", ".spot_core", function(){
+            View_Spot.core();
+        });
+        $(".view").on("click", ".spot_disable", function(){
+            View_Spot.disable();
         });
     },
 
@@ -34,6 +51,7 @@ let View = {
         txt +=     "<div class='right'>";
         txt +=          "<div class='header'><ul>";
         txt +=          "</ul></div>";
+        txt +=          "<div class='setter'><h3></h3><div class='setter__content'></div></div>";
         txt +=          "<div class='viewer'></div>";
         txt +=     "</div>";
         txt += "</div>";
@@ -43,11 +61,17 @@ let View = {
             "메트로":[
                 {ko:"지우기", en:"metro_disable"},
                 {ko:"전체 노선", en:"metro_full"},
-                {ko:"핵심 노선", en:"metro_core"},
-                {ko:"클릭 추적", en:"metro_click"}
+                {ko:"핵심 노선", en:"metro_core"}
             ],
-            "관광지":[],
-            "분석":[]
+            "관광지":[
+                {ko:"지우기", en:"spot_disable"},
+                {ko:"전체 관광지", en:"spot_full"},
+                {ko:"핵심 관광지", en:"spot_core"}
+            ],
+            "클릭 추적":[
+                {ko:"지하철 노선만",en:"click_metro"},
+                {ko:"지하철과 관광지", en:"click_metroSpot"}
+            ]
         };
 
         let hTxt = '';
@@ -73,8 +97,25 @@ let View = {
             disableDefaultUI: true,
             styles:[
                 {
+                  "featureType": "administrative",
+                  "elementType": "geometry",
+                  "stylers": [
+                    {
+                      "visibility": "off"
+                    }
+                  ]
+                },
+                {
                   "featureType": "administrative.land_parcel",
                   "elementType": "labels",
+                  "stylers": [
+                    {
+                      "visibility": "off"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "poi",
                   "stylers": [
                     {
                       "visibility": "off"
@@ -91,7 +132,16 @@ let View = {
                   ]
                 },
                 {
-                  "featureType": "poi.business",
+                  "featureType": "poi.park",
+                  "stylers": [
+                    {
+                      "visibility": "on"
+                    }
+                  ]
+                },
+                {
+                  "featureType": "poi.park",
+                  "elementType": "labels",
                   "stylers": [
                     {
                       "visibility": "off"
@@ -128,6 +178,7 @@ let View = {
         });
 
         View_Metro.map = this.map;
+        View_Spot.map = this.map;
     }
 };
 
--- app/scripts/pages/view/metro.js
+++ app/scripts/pages/view/metro.js
@@ -3,10 +3,25 @@ let View_Metro = {
     polyLine:{},
     data:{},
     markerSet: [],
+    radius:500,
 
     init: function(data){
         this.data = data;
-        console.log(data);
+
+        //기존 세팅 초기화
+        for (var line in this.polyLine) {
+            this.polyLine[line].setMap(null);
+        }
+        this.polyLine = {};
+        if(this.marker){    //기존 마커 초기화
+            this.marker.setMap(null);
+        }
+        for (let i = 0; i < this.markerSet.length; i++) {
+            this.markerSet[i].setMap(null);
+        }
+        this.markerSet = [];
+
+
         let metroLine = data.metroLine;
         for (let line in metroLine) {
             if(metroLine[line].stn){
@@ -27,6 +42,9 @@ let View_Metro = {
     },
 
     full: function(){
+        $(".view .setter h3").html('전체 노선');
+        $(".view .setter__content").html('');
+        $(".view .viewer").html('');
         if(this.marker){    //기존 마커 초기화
             this.marker.setMap(null);
         }
@@ -35,9 +53,16 @@ let View_Metro = {
         for (var line in this.polyLine) {
             this.polyLine[line].setMap(this.map);
         }
+        for (let i = 0; i < this.markerSet.length; i++) {
+            this.markerSet[i].setMap(null);
+        }
+        this.markerSet = [];
     },
 
     disable: function(){
+        $(".view .setter h3").html('');
+        $(".view .setter__content").html('');
+        $(".view .viewer").html('');
         if(this.marker){    //기존 마커 초기화
             this.marker.setMap(null);
         }
@@ -46,21 +71,68 @@ let View_Metro = {
         for (var line in this.polyLine) {
             this.polyLine[line].setMap(null);
         }
+        for (let i = 0; i < this.markerSet.length; i++) {
+            this.markerSet[i].setMap(null);
+        }
+        this.markerSet = [];
     },
 
     clickMode: function(){
+        $(".view .setter h3").html('클릭 추적 - 지하철 노선만');
         let that = this;
 
         for (var line in this.polyLine) {
             this.polyLine[line].setMap(null);
         }
+        google.maps.event.clearListeners(this.map, 'click');
         this.map.addListener('click', function(e){
             that.findMetro(e);
         });
+
+        let txt = '';
+        txt += '<div class="setter__radius">';
+        txt +=     '<span class="setter__txt">탐색반경 </span>';
+        txt +=     '<input class="setter__radius__input" value="'+this.radius+'">';
+        txt += '</div>';
+
+        $(".view .setter__content").html(txt);
     },
 
-    findMetro: function(e){
+    clickSpotMode: function(){
+        $(".view .setter h3").html('클릭 추적 - 지하철 노선과 연관 관광지');
+        let that = this;
+
+        for (var line in this.polyLine) {
+            this.polyLine[line].setMap(null);
+        }
+        google.maps.event.clearListeners(this.map, 'click');
+        this.map.addListener('click', function(e){
+            that.findMetroSpot(e);
+        });
+
+        let txt = '';
+        txt += '<div class="setter__radius">';
+        txt +=     '<span class="setter__txt">탐색반경 </span>';
+        txt +=     '<input class="setter__radius__input" value="'+this.radius+'">';
+        txt += '</div>';
+
+        $(".view .setter__content").html(txt);
+    },
+    changeRadius: function(){
+        let input = $(".setter__radius__input").val()*1;
+        if(isNaN(input)){
+            toast("숫자만 입력해주세요");
+        }else if(input>100&&input<601){
+            this.radius = input;
+        }else{
+            toast("100~600 사이로 입력해주세요");
+        }
+    },
+
+    findMetroSpot: function(e){
         let lineObj = {};  //key:라인명, value:{stn:station, dif:dif}
+        let stnArr = [];
+        let spotObj = {};
 
         let clickCoor = {
             lat:e.latLng.lat(),
@@ -87,55 +159,240 @@ let View_Metro = {
         for (let i = 0; i < metros.length; i++) {
             let metro = metros[i];
             let dif = calculateDif(clickCoor, metro.coor);
+            metro.dif = dif;
 
-            if(dif<500){
+            if(dif<this.radius){
+                stnArr.push(metro);
+
                 for (let j = 0; j < metro.line.length; j++) {
                     let line = metro.line[j];
 
-                    if(lineObj[line]){  //이미 있으면 짧은 거리로 업데이트
+                    if(lineObj[line]){  //이미 있으면 짧을 때만 덮어씌움
                         if(dif < lineObj[line].dif){
-                            lineObj[line] = {
-                                stn:metro,
-                                dif:dif
-                            };
+                            lineObj[line] = metro;
                         }
                     }else{ //없으면 새로 추가
-                        lineObj[line] = {
-                            stn:metro,
-                            dif:dif
-                        };
+                        lineObj[line] = metro;
                     }
                 }
             }
         }
 
+        stnArr.sort((a, b) => a.dif - b.dif);
+
         let txt = '';
         txt+='<div class="metro__info">';
 
-        for (let line in lineObj) {
-            this.polyLine[line].setMap(this.map);
+        for (let i = 0; i < stnArr.length; i++) {
+            let stn = stnArr[i];
 
-            let stn = lineObj[line].stn;
-            let marker = new google.maps.Marker({
-                position: stn.coor,
-                map:this.map,
-                icon: {
-                    path: google.maps.SymbolPath.CIRCLE,
-                    strokeColor: this.data.metroLine[line].color,
-                    scale: 7,
-                    strokeWeight:5
-                },
-            });
-            txt+='<div class="metro__info__line">';
-            txt+=   '<p class="metro__info__lineName" style="background:'+this.data.metroLine[line].color+'">'+line+'</p>';
-            txt+=   '<p class="metro__info__dif">'+ Math.round(lineObj[line].dif) + 'm</p>';
-            txt+=   '<p class="metro__info__stnName">'+ stn.name + '역</p>';
+            txt+='<div class="metro__info__stn">';
+
+            txt+=   '<p class="metro__info__stn__name">'+ stn.name + '역</p>';
+            txt+=   '<div class="metro__info__stn__line">';
+
+            for (let j = 0; j < stn.line.length; j++) {
+                let line = stn.line[j];
+                let color = this.data.metroLine[line].color;
+                let fontColor = "#fff";
+                if(this.data.metroLine[line].fontColor){
+                    fontColor = this.data.metroLine[line].fontColor;
+                }
+
+                let marker = new google.maps.Marker({
+                    position: stn.coor,
+                    map:this.map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        strokeColor: color,
+                        scale: (8-j*2),
+                        strokeWeight:2
+                    },
+                });
+                this.markerSet.push(marker);
+
+                txt+=   '<p class="metro__info__lineName" style="background:'+color+'; color:'+fontColor+'">'+line+'</p>';
+            }
+            txt+=   '<p class="metro__info__stn__dif">'+ Math.round(stn.dif) + 'm</p>';
+
+            txt+=   '</div>';
             txt+='</div>';
+        }
+        console.log(lineObj);
 
-            this.markerSet.push(marker);
+        for (let line in lineObj) {
+            if(this.polyLine[line]){
+                this.polyLine[line].setMap(this.map);
+                let dif = lineObj[line].dif;
+                let spots = this.data.metroLine[line].spot;
+
+                for (let i = 0; i < spots.length; i++) {
+                    let rank = spots[i].rank;
+
+                    if(this.radius > spots[i].dif){
+                        if(spotObj[rank]){
+                            let oldDif = spotObj[rank].dif.hotel + spotObj[rank].dif.spot;
+                            let newDif = spots[i].dif + dif;
+    
+                            if(oldDif > newDif){
+                                spotObj[rank] = spots[i];
+                                spotObj[rank].stn = {
+                                    spot: spots[i].stn.name,
+                                    hotel: lineObj[line].name
+                                };
+                                spotObj[rank].line = line;
+                                spotObj[rank].dif = {
+                                    hotel:dif,          //숙소에서 가까운 메트로까지의 dif
+                                    spot:spots[i].dif   //메트로에서 스팟까지의 dif
+                                };
+                            }
+                        }else{
+                            spotObj[rank] = spots[i];
+                            spotObj[rank].stn = {
+                                spot: spots[i].stn.name,
+                                hotel: lineObj[line].name
+                            };
+                            spotObj[rank].line = line;
+                            spotObj[rank].dif = {
+                                hotel:dif,          //숙소에서 가까운 메트로까지의 dif
+                                spot:spots[i].dif   //메트로에서 스팟까지의 dif
+                            };
+                        }
+                    }
+
+                }
+            }
+        }
+        console.log(spotObj);
+
+        txt += '<div class="spot__info">';
+
+        txt += '<div class="spot__info__line">';
+        txt += '<p class="spot__info__name">관광지명</p>';
+        txt += `<p class="spot__info__lineName--txt">노선</p>`;
+        txt += `<p class="spot__info__stnName">역명</p>`;
+        txt += `<p class="spot__info__distance">역에서 거리</p>`;
+        txt += '</div>';
+
+
+        for (let rank in spotObj) {
+            let spot = spotObj[rank];
+            let line = spot.line;
+            
+            let color = this.data.metroLine[line].color;
+            let fontColor = "#fff";
+            if(this.data.metroLine[line].fontColor){
+                fontColor = this.data.metroLine[line].fontColor;
+            }
+            let dif = Math.round(spot.dif.spot);
+
+            txt += '<div class="spot__info__line">';
+            txt += '<p class="spot__info__name">'+spot.name.ko+'</p>';
+            txt += `<p class="spot__info__lineName" style="background:${color}; color:${fontColor}">${line}</p>`;
+            txt += `<p class="spot__info__stnName">${spot.stn.spot}역</p>`;
+            txt += `<p class="spot__info__distance">${dif}</p>`;
+            txt += '</div>';
         }
 
         txt += '</div>';
+
+        $(".view .viewer").html(txt); 
+    },
+
+    findMetro: function(e){
+        let lineObj = {};  //key:라인명, value:{stn:station, dif:dif}
+        let stnArr = [];
+
+        let clickCoor = {
+            lat:e.latLng.lat(),
+            lng:e.latLng.lng()
+        };
+        let metros = this.data.local.metro;
+
+        if(this.marker){    //기존 마커 초기화
+            this.marker.setMap(null);
+        }
+        for (var line in this.polyLine) {   //기존 그려져있던 폴리라인 초기화
+            this.polyLine[line].setMap(null);
+        }
+        for (let i = 0; i < this.markerSet.length; i++) {
+            this.markerSet[i].setMap(null);
+        }
+        this.markerSet = [];
+
+        this.marker = new google.maps.Marker({
+            position: e.latLng,
+            map: this.map
+        });
+
+        for (let i = 0; i < metros.length; i++) {
+            let metro = metros[i];
+            let dif = calculateDif(clickCoor, metro.coor);
+            metro.dif = dif;
+
+            if(dif<this.radius){
+                stnArr.push(metro);
+
+                for (let j = 0; j < metro.line.length; j++) {
+                    let line = metro.line[j];
+
+                    if(lineObj[line]){  //이미 있으면 짧을 때만 덮어씌움
+                        if(dif < lineObj[line].dif){
+                            lineObj[line] = metro;
+                        }
+                    }else{ //없으면 새로 추가
+                        lineObj[line] = metro;
+                    }
+                }
+            }
+        }
+
+        stnArr.sort((a, b) => a.dif - b.dif);
+
+        let txt = '';
+        txt+='<div class="metro__info">';
+
+        for (let i = 0; i < stnArr.length; i++) {
+            let stn = stnArr[i];
+
+            txt+='<div class="metro__info__stn">';
+
+            txt+=   '<p class="metro__info__stn__name">'+ stn.name + '역</p>';
+            txt+=   '<div class="metro__info__stn__line">';
+
+            for (let j = 0; j < stn.line.length; j++) {
+                let line = stn.line[j];
+                let color = this.data.metroLine[line].color;
+                let fontColor = "#fff";
+                if(this.data.metroLine[line].fontColor){
+                    fontColor = this.data.metroLine[line].fontColor;
+                }
+
+                let marker = new google.maps.Marker({
+                    position: stn.coor,
+                    map:this.map,
+                    icon: {
+                        path: google.maps.SymbolPath.CIRCLE,
+                        strokeColor: color,
+                        scale: (8-j*2),
+                        strokeWeight:2
+                    },
+                });
+                this.markerSet.push(marker);
+
+                txt+=   '<p class="metro__info__lineName" style="background:'+color+'; color:'+fontColor+'">'+line+'</p>';
+            }
+            txt+=   '<p class="metro__info__stn__dif">'+ Math.round(stn.dif) + 'm</p>';
+
+            txt+=   '</div>';
+            txt+='</div>';
+        }
+
+        for (let line in lineObj) {
+            if(this.polyLine[line]){
+                this.polyLine[line].setMap(this.map);
+            }
+        }
 
         $(".view .viewer").html(txt);
 
